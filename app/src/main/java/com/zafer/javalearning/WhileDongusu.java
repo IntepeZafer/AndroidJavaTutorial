65c7c0e6e73be809649e4b65773cfee9bb7ef716
IntepeZafer <intepezafer@gmail.com>
2023-10-22 14:07:42 +0300

    Complate

--- app/src/main/java/com/zafer/javalearning/WhileDongusu.java
+++ app/src/main/java/com/zafer/javalearning/WhileDongusu.java
@@ -2,6 +2,10 @@ package com.zafer.javalearning;
 
 public class WhileDongusu {
     public static void main(String[] args){
-        
+        int x = 0;
+        while (x < 10){
+            System.out.println(x);
+            x++;
+        }
     }
 }
